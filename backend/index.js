75e90cc75c96061308240d2a4529151a2306a07d
AKASH89-19 <scaku8960@gmail.com>
2026-04-14 18:36:11 +0530

    what you changed

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,16 @@
 //organization
 //issue
 //board database
+const express = require("express")
 const user =[];
 const organization=[];
 const issue = [];
+const app = express()
+app.get("/", (req, res) => {
+    res.send("Server is working!")
+})
+
+app.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000")
+})
+
